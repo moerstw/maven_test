4bbde5071f7742ce9021001b0047605cf63393bb
moerstw@gmail.com <moerstw@gmail.com>
2015-03-01 19:44:37 +0000

    hadoop test9

---
+++ src/main/java/org/moerstw/hadoop/TestHelloWorld.java
@@ -0,0 +1,18 @@
+package org.moerstw.hadoop;
+import org.joda.time.LocalTime;
+
+public class TestHelloWorld {
+  
+  public static class Greeter {
+    public String sayHello() {
+        return "Hello world!";
+    }
+  }
+
+  public static void main(String args[]) {
+    LocalTime currentTime = new LocalTime();
+    System.out.println("The current local time is: " + currentTime);
+    Greeter greeter = new Greeter();
+    System.out.println(greeter.sayHello());
+  }  
+}

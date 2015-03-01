71c989df5bf7ed57398a42a70f83b851fd432426
moerstw@gmail.com <moerstw@gmail.com>
2015-03-01 14:16:34 +0000

    init

---
+++ src/main/java/org/example/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    SayHello hiThere = new SayHello();
+    System.out.println(hiThere.sayHello());
+  };
+}
+
+
---
+++ src/main/java/org/example/SayHello.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class SayHello {
+  public String sayHello() {
+    return "Hello World!";
+  }
+}

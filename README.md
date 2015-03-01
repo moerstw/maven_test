5e0a684cd48b0c42189e31c954fe9a492f0b5ea6
moerstw@gmail.com <moerstw@gmail.com>
2015-03-01 22:10:30 +0000

    wordcount mapper done!

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # maven_test
 This is maven test README
 
-
+apt-get install maven
+mvn clean
 mvn compile
 mvn package
-mvn install
--- src/main/java/org/moerstw/hadoop/WordCount.java
+++ src/main/java/org/moerstw/hadoop/WordCount.java
@@ -1,18 +1,43 @@
 package org.moerstw.hadoop;
-import org.joda.time.LocalTime;
+//import org.joda.time.LocalTime;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Mapper;
 
 public class WordCount {
-  
-  public static class Greeter {
-    public String sayHello() {
-        return "Hello world!";
+  public static class WordCountMap extends Mapper<LongWritable, Text, Text, IntWritable> {
+    private final static IntWritable one  = new IntWritable(1);
+    private Text word = new Text();
+    @Override
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+      String line = value.toString();
+      StringTokenizer tokenizer = new StringTokenizer(line);
+      while (tokenizer.hasMoreTokens()) {
+        word.set(tokenizer.nextToken());
+        context.write(word, one);
+      }
     }
-  }
+    
+    @Override
+    public void run(Context context) throws IOException, InterruptedException {
+      setup(context);  // Override
+      while (context.nextKeyValue()) {
+        map(context.getCurrentKey(), context.getCurrentValue(), context);
+      }
+      cleanup(context);  // Override
+    }
+  } 
 
   public static void main(String args[]) {
-    LocalTime currentTime = new LocalTime();
-    System.out.println("The current local time is: " + currentTime);
-    Greeter greeter = new Greeter();
-    System.out.println(greeter.sayHello());
+    System.out.println("madada");
   }  
 }
+
+ /**
+  * Reference
+  * http://www.aichengxu.com/view/39755
+  * http://blog.puneethabm.in/wordcount-mapreduce-program-using-hadoop-new-api-2/
+  */
